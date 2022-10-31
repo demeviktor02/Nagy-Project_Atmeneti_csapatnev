52aa3def54c332d1c7027b6b2ffee643932d6ff1
demeviktor02 <114135525+demeviktor02@users.noreply.github.com>
2022-10-31 14:10:57 +0100

    Program - Esemény adatbázis létrehozása

---
+++ Calendar/app/src/main/java/com/example/calendar/DBOpenHelper.java
@@ -0,0 +1,25 @@
+package com.example.calendar;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+
+    public DBOpenHelper(@Nullable Context context) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
