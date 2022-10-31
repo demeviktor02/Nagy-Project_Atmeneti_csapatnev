95eaa2be2bbc8b9362c13a00f2d89adff3f39a99
demeviktor02 <114135525+demeviktor02@users.noreply.github.com>
2022-10-31 13:53:40 +0100

    Program - naptár programozás alapok

--- Calendar/app/src/main/java/com/example/calendar/CustomCalendarView.java
+++ Calendar/app/src/main/java/com/example/calendar/CustomCalendarView.java
@@ -1,6 +1,7 @@
 package com.example.calendar;
 
 import android.content.Context;
+import android.provider.CalendarContract;
 import android.util.AttributeSet;
 import android.widget.GridView;
 import android.widget.ImageButton;
@@ -9,7 +10,10 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class CustomCalendarView extends LinearLayout{
@@ -19,7 +23,9 @@ public class CustomCalendarView extends LinearLayout{
     private static final int MAX_CALENDAR_DAYS = 42;
     Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
     Context context;
-    
+
+    List<Date> dates = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
 
     public CustomCalendarView(Context context) {
         super(context);
---
+++ Calendar/app/src/main/java/com/example/calendar/Events.java
@@ -0,0 +1,5 @@
+package com.example.calendar;
+
+public class Events {
+    String EVENT,TIME,DATE,MONTH,YEAR;
+}
