fda39d18b2a7e2eea361753b8a41a7f6899a5428
katonaBalintSandor <balintgamer14@gmail.com>
2022-11-09 17:18:30 +0100

    Program - MyViewHolder osztály létrehozása

--- Calendar/app/src/main/java/com/example/calendar/EventRecyclerAdapter.java
+++ Calendar/app/src/main/java/com/example/calendar/EventRecyclerAdapter.java
@@ -7,6 +7,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdapter.MyViewHolder>{
 
+    Context context;
+    ArrayList<Events> arrayList;
+
+    public EventRecyclerAdapter(Context context,ArrayList<Events> arrayList){
+        this.context=context;
+        this.arrayList=arrayList;
+    }
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent,int viewType){
+        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.show_events_rowlayout,parent, attachToRoot false);
+
+        return new MyViewHolder(view);
+    }
+
+    public onBindViewHolder(@NonNull MyViewHolder holder,int position){
+        Events events=arrayList.get(position);
+        holder.Event.setText(events.getEVENT());
+        holder.DateTxt.setText(events.getDATE());
+        holder.Time.setText(events.getTIME());
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView DateTxt,Event,Time;
 
