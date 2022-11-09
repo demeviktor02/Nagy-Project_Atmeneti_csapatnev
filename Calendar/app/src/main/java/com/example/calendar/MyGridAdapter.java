174f10b261209fe9981c58f30501ec0e72a203dd
katonaBalintSandor <balintgamer14@gmail.com>
2022-11-09 15:13:53 +0100

    Program - MyGridAdapter osztály létrehozása

---
+++ Calendar/app/src/main/java/com/example/calendar/MyGridAdapter.java
@@ -0,0 +1,8 @@
+package com.example.calendar;
+
+public class MyGridAdapter extends ArrayAdapter
+{
+    public MyGridAdapter(@NonNull Context context, int resource) {
+        super(context,resource);
+    }
+}
