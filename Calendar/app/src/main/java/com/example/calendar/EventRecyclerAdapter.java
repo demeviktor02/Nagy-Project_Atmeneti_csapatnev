b92c201ae801bb68e0d32974ee963b221c1bf010
Adam <balint.adam.git@gmail.com>
2022-11-30 20:21:47 +0100

    Program - EventRecyclerAdapter hibajavítás

--- Calendar/app/src/main/java/com/example/calendar/EventRecyclerAdapter.java
+++ Calendar/app/src/main/java/com/example/calendar/EventRecyclerAdapter.java
@@ -1,9 +1,16 @@
 package com.example.calendar;
 
 import androidx.annotation.NonNull;
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdapter.MyViewHolder>{
 
@@ -15,25 +22,37 @@ public class EventRecyclerAdapter extends RecyclerView.Adapter<EventRecyclerAdap
         this.arrayList=arrayList;
     }
 
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent,int viewType){
-        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.show_events_rowlayout,parent, attachToRoot false);
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.show_events_rowlayout,parent, false);
 
         return new MyViewHolder(view);
     }
 
-    public onBindViewHolder(@NonNull MyViewHolder holder,int position){
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder,int position){
         Events events=arrayList.get(position);
         holder.Event.setText(events.getEVENT());
         holder.DateTxt.setText(events.getDATE());
         holder.Time.setText(events.getTIME());
     }
 
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView DateTxt,Event,Time;
 
+        Button delete;
+
+
+
         public MyViewHolder(@NonNull View itemView){
             super(itemView);
-            DateTxt=itemView.findViewById(R.id.);
+            DateTxt=itemView.findViewById(R.id.eventdate);
             Event=itemView.findViewById(R.id.eventname);
             Time=itemView.findViewById(R.id.eventime);
         }
